4f50f4ffc15a9bd19f993cf8443df344a5b414ab
Amar <74137093+Amar-Mahmoud@users.noreply.github.com>
2023-04-11 01:27:17 -0400

    footer

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import "./Footer.css";
+import IconButton from "@mui/material/IconButton";
+import Instagram from "@mui/icons-material/Instagram";
+import Facebook from "@mui/icons-material/Facebook";
+import Twitter from "@mui/icons-material/Twitter";
+import Linkedin from "@mui/icons-material/LinkedIn";
+
+const Footer = () => (
+  <Paper
+    style={{
+      marginTop: "20px",
+      width: "100%",
+      height: "7%",
+      position: "fixed",
+      bottom: 0,
+      backgroundColor: "#363636",
+    }}
+    component="footer"
+    square
+    variant="outlined"
+  >
+    <div className="aParent">
+      <div style={{ color: "white", margin: "1%" }}>
+        <span>
+          © 2023 WeekendAllWeek <br /> All Rights Reserved
+        </span>
+      </div>
+      <div
+        style={{
+          margin: "2%",
+          float: "right",
+          display: "inline-block",
+          fontFamily: "sans-serif",
+        }}
+      >
+        <span>
+          <Button style={{ color: "white" }} size="small">
+            Privacy Policy
+          </Button>
+          <Button style={{ color: "white" }} size="small">
+            Terms of Use
+          </Button>
+          <Button style={{ color: "white" }} size="small">
+            Sales and Refunds
+          </Button>
+          <Button style={{ color: "white" }} size="small">
+            Legal
+          </Button>
+          <Button style={{ color: "white" }} size="small">
+            Site Map
+          </Button>
+          <IconButton
+            style={{ color: "white", fontSize: 40 }}
+            component={Facebook}
+            inheritViewBox
+          />
+          <IconButton
+            style={{ color: "white", fontSize: 40 }}
+            component={Twitter}
+            inheritViewBox
+          />
+          <IconButton
+            style={{ color: "white", fontSize: 40 }}
+            component={Instagram}
+            inheritViewBox
+          />
+          <IconButton
+            style={{ color: "white", fontSize: 40 }}
+            component={Linkedin}
+            inheritViewBox
+          />
+          <br />
+        </span>
+      </div>
+    </div>
+  </Paper>
+);
+
+export default Footer;
